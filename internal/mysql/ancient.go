cef4e7a37a161d4cc5284d4da5c953b6e8476df6
hatlonely <hatlonely@gmail.com>
2019-09-19 16:47:50 +0800

    update

--- internal/mysql/ancient.go
+++ internal/mysql/ancient.go
@@ -6,10 +6,10 @@ import (
 )
 
 type Ancient struct {
-	ID      int    `gorm:"type:bigint(20) auto_increment;primary_key" json:"id"`
-	Title   string `gorm:"type:varchar(64);index:title_idx;unique_index:tad_idx;not null" json:"title,omitempty"`
-	Author  string `gorm:"type:varchar(64);index:author_idx;unique_index:tad_idx;not null" json:"author,omitempty"`
-	Dynasty string `gorm:"type:varchar(32);index:dynasty_idx;unique_index:tad_idx;not null" json:"dynasty,omitempty"`
+	ID      int    `gorm:"type:bigint(20);primary_key" json:"id"`
+	Title   string `gorm:"type:varchar(64);index:title_idx;not null" json:"title,omitempty"`
+	Author  string `gorm:"type:varchar(64);index:author_idx;not null" json:"author,omitempty"`
+	Dynasty string `gorm:"type:varchar(32);index:dynasty_idx;not null" json:"dynasty,omitempty"`
 	Content string `gorm:"type:longtext COLLATE utf8mb4_unicode_520_ci;not null" json:"content,omitempty"`
 }
 
