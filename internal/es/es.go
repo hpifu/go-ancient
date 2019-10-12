4cc5bfb97c7c943a409eb259d9830494eb6c561d
hatlonely <hatlonely@gmail.com>
2019-10-13 04:47:06 +0800

    update

--- internal/es/es.go
+++ internal/es/es.go
@@ -2,9 +2,66 @@ package es
 
 import (
 	"context"
-	"github.com/olivere/elastic/v7"
+	"fmt"
 	"time"
+
+	"github.com/olivere/elastic/v7"
 )
+
+var ancientMapping = `{
+    "settings": {
+        "analysis": {
+            "tokenizer": {
+                "ngram_tokenizer": {
+                    "type": "nGram",
+                    "min_gram": 1,
+                    "max_gram": 10,
+                    "token_chars": [
+                        "letter",
+                        "digit"
+                    ]
+                }
+            },
+            "analyzer": {
+                "ngram_tokenizer_analyzer": {
+                    "type": "custom",
+                    "tokenizer": "ngram_tokenizer",
+                    "filter": [
+                        "lowercase"
+                    ]
+                }
+            }
+        },
+        "max_ngram_diff": "10"
+	},
+	"mappings": {
+		"properties": {
+			"id": {
+				"type": "long"
+			},
+			"title": {
+				"type": "text",
+				"analyzer": "ngram_tokenizer_analyzer",
+				"search_analyzer": "standard"
+			},
+			"author": {
+				"type": "text",
+				"analyzer": "ngram_tokenizer_analyzer",
+				"search_analyzer": "standard"
+			},
+			"dynasty": {
+				"type": "text",
+				"analyzer": "ngram_tokenizer_analyzer",
+				"search_analyzer": "standard"
+			},
+			"content": {
+				"type": "text",
+				"analyzer": "ngram_tokenizer_analyzer",
+				"search_analyzer": "standard"
+			}
+		}
+	}
+}`
 
 type ES struct {
 	es *elastic.Client
@@ -20,11 +77,31 @@ func NewES(uri string) (*ES, error) {
 		return nil, err
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 3000*time.Millisecond)
-	defer cancel()
-	_, _, err = client.Ping(uri).Do(ctx)
-	if err != nil {
-		return nil, err
+	{
+		ctx, cancel := context.WithTimeout(context.Background(), 3000*time.Millisecond)
+		defer cancel()
+		_, _, err = client.Ping(uri).Do(ctx)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	{
+		ctx, cancel := context.WithTimeout(context.Background(), 3000*time.Millisecond)
+		defer cancel()
+		exists, err := client.IndexExists("ancient").Do(ctx)
+		if err != nil {
+			return nil, err
+		}
+		if !exists {
+			createIndex, err := client.CreateIndex("ancient").Body(ancientMapping).Do(context.Background())
+			if err != nil {
+				return nil, err
+			}
+			if !createIndex.Acknowledged {
+				return nil, fmt.Errorf("not acknowledged")
+			}
+		}
 	}
 
 	return &ES{
